d261ab7f9389f73b4b59e0ef0b896f2a086887aa
Nick Gowdy <nickgowdy@nicks-mbp.home>
2022-04-10 11:59:21 +0100

    Basic navigation working, can refactor later.

--- src/models/marsRover.go
+++ src/models/marsRover.go
@@ -11,7 +11,7 @@ type MarsRover struct {
 }
 
 func NewRover(x int, y int, direction byte) (*MarsRover, error) {
-	err := HandleBadInput(direction)
+	err := HandleBadDirection(direction)
 
 	if err != nil {
 		return nil, err
@@ -21,7 +21,68 @@ func NewRover(x int, y int, direction byte) (*MarsRover, error) {
 	return &m, nil
 }
 
-func HandleBadInput(direction byte) error {
+func Move(marsRover *MarsRover, instructions string) error {
+	parsedInstuctions := []byte(instructions)
+
+	for i := 0; i < len(parsedInstuctions); i++ {
+		instruction := string(parsedInstuctions[i])
+
+		if instruction == "L" || instruction == "R" {
+			Turn(marsRover, instruction)
+		} else if instruction == "M" {
+			Forward(marsRover, instruction)
+		} else {
+			return errors.New("Bad input")
+		}
+	}
+
+	return nil
+}
+
+func Turn(marsRover *MarsRover, instruction string) {
+	switch marsRover.Direction {
+	case 'N':
+		if instruction == "L" {
+			marsRover.Direction = 'W'
+		} else {
+			marsRover.Direction = 'E'
+		}
+	case 'S':
+		if instruction == "L" {
+			marsRover.Direction = 'E'
+		} else {
+			marsRover.Direction = 'W'
+		}
+	case 'E':
+		if instruction == "L" {
+			marsRover.Direction = 'N'
+		} else {
+			marsRover.Direction = 'S'
+		}
+	case 'W':
+		if instruction == "L" {
+			marsRover.Direction = 'S'
+		} else {
+			marsRover.Direction = 'N'
+		}
+	}
+
+}
+
+func Forward(marsRover *MarsRover, instruction string) {
+	switch marsRover.Direction {
+	case 'N':
+		marsRover.Y++
+	case 'S':
+		marsRover.Y--
+	case 'E':
+		marsRover.X++
+	case 'W':
+		marsRover.X--
+	}
+}
+
+func HandleBadDirection(direction byte) error {
 	if direction == 'N' || direction == 'S' || direction == 'E' || direction == 'W' {
 		return nil
 	}
--- src/models/marsRover_test.go
+++ src/models/marsRover_test.go
@@ -17,3 +17,19 @@ func TestNewMarsRoverDirection(t *testing.T) {
 		t.Error("Mars Rover directions can only be N, S, E, W")
 	}
 }
+
+func TestMarsRoverMove(t *testing.T) {
+	rover, _ := NewRover(1, 2, 'N')
+
+	instructions := "LMLMLMLMM"
+
+	Move(rover, instructions)
+
+	if rover.X != 1 {
+		t.Error("Rover X position should be 1")
+	}
+
+	if rover.Y != 3 {
+		t.Error("Rover Y position should be 1")
+	}
+}
