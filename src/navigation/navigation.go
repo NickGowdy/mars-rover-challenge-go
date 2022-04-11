c36e1e67fe3b856ae4f12dcf17ab885ef7d03403
Nick Gowdy <nickgowdy@nicks-mbp.home>
2022-04-11 11:03:51 +0100

    TODO: calculate if possible collision.

--- src/navigation/navigation.go
+++ src/navigation/navigation.go
@@ -1,7 +1,67 @@
 package navigation
 
-import "fmt"
+import (
+	"mars_rover/src/models"
+)
 
-func Do() {
-	fmt.Println("Init rover")
+func Move(marsRovers []*models.MarsRover) {
+
+	for _, marsRover := range marsRovers {
+		for _, c := range marsRover.Instructions {
+
+			if string(c) == "L" || string(c) == "R" {
+				turn(marsRover, string(c))
+			} else if string(c) == "M" && noPossibleCollision(marsRover, marsRovers) {
+				forward(marsRover, string(c))
+			}
+		}
+	}
+}
+
+func noPossibleCollision(marsRover *models.MarsRover, marsRovers []*models.MarsRover) bool {
+	// TODO: calculate if possible collision
+	return true
+}
+
+func turn(marsRover *models.MarsRover, instruction string) {
+	switch marsRover.Direction {
+	case "N":
+		if instruction == "L" {
+			marsRover.Direction = "W"
+		} else {
+			marsRover.Direction = "E"
+		}
+	case "S":
+		if instruction == "L" {
+			marsRover.Direction = "E"
+		} else {
+			marsRover.Direction = "W"
+		}
+	case "E":
+		if instruction == "L" {
+			marsRover.Direction = "N"
+		} else {
+			marsRover.Direction = "S"
+		}
+	case "W":
+		if instruction == "L" {
+			marsRover.Direction = "S"
+		} else {
+			marsRover.Direction = "N"
+		}
+	}
+
+}
+
+func forward(marsRover *models.MarsRover, instruction string) {
+	switch marsRover.Direction {
+	case "N":
+		marsRover.Y++
+	case "S":
+		marsRover.Y--
+	case "E":
+		marsRover.X++
+	case "W":
+		marsRover.X--
+	}
 }
--- src/navigation/navigation_test.go
+++ src/navigation/navigation_test.go
@@ -1,9 +1,25 @@
 package navigation
 
 import (
+	"mars_rover/src/models"
 	"testing"
 )
 
 func TestNavigation(t *testing.T) {
 
+	rover, _ := models.NewRover(1, 2, "N", "LMLMLMLMM")
+
+	Move([]*models.MarsRover{rover})
+
+	if rover.X != 1 {
+		t.Error("Rover X coordinate should be 1")
+	}
+
+	if rover.Y != 3 {
+		t.Error("Rover Y coordinate should be 1")
+	}
+
+	if rover.Direction != "N" {
+		t.Error("Rover should be facing direction N")
+	}
 }
