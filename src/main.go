6fbb802000c4af346c4e0664ca0c15ece4924364
Nick Gowdy <nickgowdy@nicks-mbp.home>
2022-04-11 12:27:54 +0100

    Adding code to main.go

--- src/main.go
+++ src/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"mars_rover/src/models"
+	"mars_rover/src/navigation"
+)
 
+func main() {
+	rover1, _ := models.NewRover(1, 2, "N", "LMLMLMLMM")
+	rover2, _ := models.NewRover(3, 3, "E", "MMRMMRMRRM")
+
+	navigation.Move([]*models.MarsRover{rover1, rover2}, 5, 5)
 }
